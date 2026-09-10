3337e0222df512b8df9741aa1204c10925126bd5
Rauli Laine <rauli.laine@treet.fi>
2026-09-10 11:21:00 +0300

    Use warmer colorscheme

--- init.lua
+++ init.lua
@@ -65,7 +65,7 @@ require("lazy").setup({
       priority = 1000, -- make sure to load this before all the other start plugins
       config = function()
         require('onedark').setup {
-          style = 'darker'
+          style = 'warmer'
         }
         require('onedark').load()
       end
